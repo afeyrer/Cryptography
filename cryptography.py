96b1c4df1ba5d83b633a3160d84b04fe30189313
afeyrer <abigailfeyrer@hanovernorwichschools.org>
2017-03-06 14:25:33 -0500

    Updated from Brython Server: 3/6/2017 2:25:33 PM

--- cryptography.py
+++ cryptography.py
@@ -1,7 +1,7 @@
 """
 cryptography.py
-Author: <your name here>
-Credit: <list sources used, if any>
+Author: Abby Feyrer
+Credit: None
 
 Assignment:
 
@@ -10,3 +10,17 @@ Write and submit a program that encrypts and decrypts user data.
 See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
 """
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
+z=0
+while z==0:
+    opt=input("Enter e to encrypt, d to decrypt, or q to quit: ")
+    if opt=="e":
+        mes=input("Message: ")
+        key=input("Key: ")
+    if opt=="d":
+        print(opt)
+    if opt=="q":
+        print("Goodbye!")
+        z=1
+        
+    if opt!="q" and opt!="e" and opt!="d":
+        print("Did not understand command, try again.")
